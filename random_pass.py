622304bde09245e4878832bb9ad8665c552eab07
Anargyros Tsadimas <argiris@norbloc.com>
2021-02-28 18:14:09 +0200

    fix issue #1

--- random_pass.py
+++ random_pass.py
@@ -6,11 +6,11 @@ def get_random_string(length):
     # choose from all lowercase letter
     letters = string.ascii_lowercase
     result_str = ''.join(random.choice(letters) for i in range(length))
-    return f"Random string of length {length} is: {result_str}"
+    return result_str
 
 def main():
     pass_length = int(sys.argv[1]) if len(sys.argv) >=2 else 8
-    print(get_random_string(pass_length))
+    print(f"Random string of length {pass_length} is {get_random_string(pass_length)}")
 
 if __name__ == "__main__":
     main()
