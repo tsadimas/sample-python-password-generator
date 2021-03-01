dbc02711cafda6571b4d141527d4856591593d4a
Anargyros Tsadimas <tsadimas@hua.gr>
2021-03-01 11:28:19 +0200

    fix test

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class TestRand(unittest.TestCase):
     def test_random(self):
         result = get_random_string(8)
         print(result)
-        self.assertEqual(len(result),38)
+        self.assertEqual(len(result),8)
 
 if __name__ == '__main__':
     unittest.main()
